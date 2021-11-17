e9768ba9091fa4aaa10917746286a96ba756e746
Jose Martinez <jose.martinez@cit.hn>
2021-11-16 21:23:08 -0600

    +added northwind context

---
+++ NorthwindNetCoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindDatabase.Context;
+
+namespace NorthwindNetCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        protected NorthwindDBContext _context;
+        public CustomerController(NorthwindDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public async Task<IActionResult> TestingUp()
+        {
+            var a = await _context.Customers.ToListAsync();
+            return Ok("Great!");
+        }
+    }
+}
--- NorthwindNetCoreAPI/Controllers/WeatherForecastController.cs
+++ NorthwindNetCoreAPI/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NorthwindDatabase.Context;
 
 namespace NorthwindNetCoreAPI.Controllers
 {
@@ -11,6 +12,7 @@ namespace NorthwindNetCoreAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
